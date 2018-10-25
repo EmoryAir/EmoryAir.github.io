a9d1fc11c77e4f4754e43967746172c005eaca44
morutkin <momo.rutkin@emory.edu>
2018-10-25 01:08:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -12,6 +12,6 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
+L.marker([33.5, -84]).addTo(map)
 		.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
 		.openPopup();
