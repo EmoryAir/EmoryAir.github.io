7cd2194a18eb4d33006449b2ee730b1bfd41f71d
morutkin <momo.rutkin@emory.edu>
2018-10-31 15:29:19 -0400

    changed the map to include clusters + color

--- main.js
+++ main.js
@@ -6,12 +6,43 @@
  The source code below is the example from the leaflet start page.
  */
 
-var map = L.map('map').setView([33.505, -84.09], 13);
-
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([33.5, -84]).addTo(map)
-		.bindPopup('point')
-		.openPopup();
+//var map = L.map('map').setView([33.505, -84.09], 13);
+var map = L.map( 'map', {
+	center: [33.7925,-84.3240],
+	minZoom: 9,
+	zoom: 9
+  });
+  
+  L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+   subdomains: ['a','b','c']
+  }).addTo( map );
+  
+  var myURL = jQuery( 'script[src$="main.js"]' ).attr( 'src' ).replace( 'main.js', '' );
+  
+  var myIcon = L.icon({
+	iconUrl: myURL + 'pin48.png',
+	iconRetinaUrl: myURL + 'pin48.png',
+	iconSize: [29, 24],
+	iconAnchor: [9, 21],
+	popupAnchor: [0, -14]
+  });
+  
+  var markerClusters = L.markerClusterGroup();
+  
+  for ( var i = 0; i < markers.length; ++i )
+  {
+	var popup = markers[i].name +
+				'<br/>' + markers[i].city +
+				'<br/><b>IATA/FAA:</b> ' + markers[i].iata_faa +
+				'<br/><b>ICAO:</b> ' + markers[i].icao +
+				'<br/><b>Altitude:</b> ' + Math.round( markers[i].alt * 0.3048 ) + ' m' +
+				'<br/><b>Timezone:</b> ' + markers[i].tz;
+  
+	var m = L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} )
+					.bindPopup( popup );
+  
+	markerClusters.addLayer( m );
+  }
+  
+  map.addLayer( markerClusters );
