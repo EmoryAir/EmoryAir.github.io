288ad8a77b339dc3e047291c594c3b36199b254b
amunslow <44882153+amunslow@users.noreply.github.com>
2019-03-10 21:13:55 -0400

    Add toggle to graph

--- mscData.js
+++ mscData.js
@@ -601,3 +601,13 @@
 		
 		
 	}
+
+function toggleDataSeries(e) {
+	if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+		e.dataSeries.visible = false;
+	} else {
+		e.dataSeries.visible = true;
+	}
+	e.chart.render();
+}	
+
